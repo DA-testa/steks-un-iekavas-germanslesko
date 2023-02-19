6adca7485c4eb01a5ad798d2e6c8519c67be9a6e
germanslesko <125831456+germanslesko@users.noreply.github.com>
2023-02-19 23:30:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,50 @@
+# python3
+
 from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
+
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
+
 
 def find_mismatch(text):
     opening_brackets_stack = []
     for a, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next, a + 1))
+
         if next in ")]}":
             if not opening_brackets_stack:
                 return a + 1
             top = opening_brackets_stack.pop()
             if not are_matching(top.char, next):
-
                 return a + 1
+
     if opening_brackets_stack:
         return opening_brackets_stack[0].position
-
 
     return "Success"
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    print(mismatch)
+        do=input("F or I")
+        if "F" in do:
+            name = input("Enter file name: ")
+            with open(name, "r", encoding="latinl") as file:
+                text=file.read()
+            mismatch = find_mismatch(text)
+            if mismatch == "Success":
+                print("Success")
+            else:
+                print(mismatch)
+        elif "I" in do:
+            text = input()
+            mismatch = find_mismatch(text)
+            if mismatch == "Sucess":
+                print("Success")
+            else:
+                print(mismatch)
 
 if __name__ == "__main__":
     main()
